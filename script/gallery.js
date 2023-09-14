e31a492dd7c7a6eaa37e377be34f8f05adad9734
LRod <luisjrodb@gmail.com>
2023-09-14 15:25:02 -0500

    Gallery version 2

--- script/gallery.js
+++ script/gallery.js
@@ -2,76 +2,41 @@
 const root = document.querySelector('.image-gallery');
 
 axios.get('https://api.miamibeachcocktails.com/products?location=1')
-.then((response) => {
+  .then((response) => {
 
     const data = response.data;
 
     let enableds = data.filter(product => isValidURL(product.image));
 
-  // Crear un div para contenedor de todas las columnas
-  const columnContainer = document.createElement('div');
-  columnContainer.classList.add('columns-container');
-  
-  // Variable para rastrear el número de productos por columna
-  let productosPorColumna = 0;
-  
-  // Variable para rastrear la columna actual
-  let columnaActual = null;
-  
-  // Iterar a través de los productos
-   enableds.forEach(producto => {
-    // Crear una nueva columna si es el primer producto de la columna
-    if (productosPorColumna === 0) {
-      columnaActual = document.createElement('div');
-      columnaActual.classList.add('column');
-      columnContainer.appendChild(columnaActual);
-    }
-  
-    // Crear un div de imagen con overlay para cada producto
-    const imageItem = document.createElement('div');
-    imageItem.classList.add('image-item');
-  
-    // Crear la imagen
-    const image = document.createElement('img');
-    console.log(producto.image);
-    image.src = producto.image;
-    image.alt = '';
-  
-    // Crear el div de overlay
-    const overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-  
-    // Crear el span con el título del producto
-    const titleSpan = document.createElement('span');
-    titleSpan.textContent = producto.name;
-  
-    // Agregar la imagen y el título al div de overlay
-    overlay.appendChild(image);
-    overlay.appendChild(titleSpan);
-  
-    // Agregar el div de overlay al div de imagen
-    imageItem.appendChild(overlay);
-  
-    // Agregar el div de imagen a la columna actual
-    columnaActual.appendChild(imageItem);
-  
-    // Incrementar el contador de productos por columna
-    productosPorColumna++;
-  
-    // Si ya hemos agregado tres productos a la columna, reiniciamos el contador
-    if (productosPorColumna === 3) {
-      productosPorColumna = 0;
-    }
+    // Obtener el contenedor
+    const gridContainer = document.getElementById("grid-container");
 
-      // Agregar el contenedor de columnas al documento
-    root.appendChild(columnContainer);
-})
-},(error) => {
-  console.log(error);
-});
+    // Recorrer los datos de la API y crear elementos para cada producto
+    apiData.forEach(product => {
+      // Crear un elemento <article>
+      const article = document.createElement("article");
+
+      // Asignar clases al elemento <article>
+      article.className = `beach ${product.category} ${product.location}`;
+
+      // Crear un elemento <img> y asignar el atributo src
+      const img = document.createElement("img");
+      img.src = product.image;
+      img.className = "img-responsive";
+
+      // Agregar la imagen al elemento <article>
+      article.appendChild(img);
+
+      // Agregar el elemento <article> al contenedor
+      gridContainer.appendChild(article);
+
+    })
+  }, (error) => {
+    console.log(error);
+  });
 
 function isValidURL(str) {
-    // Expresión regular para validar URLs
-    const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-    return urlPattern.test(str);
+  // Expresión regular para validar URLs
+  const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+  return urlPattern.test(str);
 }
